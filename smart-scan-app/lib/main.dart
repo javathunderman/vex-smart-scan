d1ad0fc56d0bd34b6ed0f3d90aa8a9d5240c905e
javathunderman <javathunderman@outlook.com>
2019-09-07 11:03:40 -0400

    UI improvements to tournament selector

--- smart-scan-app/lib/main.dart
+++ smart-scan-app/lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-void main() => runApp(MyApp());
-String sku;
-String dropdownvalue;
-class MyApp extends StatelessWidget {
 
+void main() => runApp(MyApp());
+String team;
+String dropdownvalue;
+String sku;
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,11 +18,11 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 class MyCustomForm extends StatefulWidget {
   @override
   _MyCustomFormState createState() => _MyCustomFormState();
 }
+
 class _MyCustomFormState extends State<MyCustomForm> {
   final myController = TextEditingController();
 
@@ -33,65 +35,60 @@ class _MyCustomFormState extends State<MyCustomForm> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Smart Scan Rankings'),
-        actions:  <Widget>[
-      // action button
-      IconButton(
-      icon: Icon(Icons.info),
-      onPressed: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => infoPage()),
-        );
-      },
-    ),
-    ]
-      ),
+      appBar: AppBar(title: Text('Smart Scan Rankings'), actions: <Widget>[
+        // action button
+        IconButton(
+          icon: Icon(Icons.info),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => infoPage()),
+            );
+          },
+        ),
+      ]),
       body: Container(
-      child: Column(children: <Widget>
-        [Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Text(
-            'Enter the last four digits of the tournament SKU: ',
+        child: Column(children: <Widget>[
+          Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Enter the a team ID to look up events: ',
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              )),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: myController,
+            ),
+          ),
+          Text(
+            'Season selector: ',
             textAlign: TextAlign.center,
             overflow: TextOverflow.ellipsis,
             style: TextStyle(fontWeight: FontWeight.bold),
-          )
-        ),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: TextField(
-            controller: myController,
           ),
-        ),
-        Text(
-          'Season selector: ',
-          textAlign: TextAlign.center,
-          overflow: TextOverflow.ellipsis,
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-      DropdownButton<String>(
-        value: dropdownvalue,
-        onChanged: (String newValue) {
-          setState(() {
-            dropdownvalue = newValue;
-          });
-        },
-        items: <String>['Tower Takeover', 'Turning Point', 'In the Zone']
-            .map<DropdownMenuItem<String>>((String value) {
-          return DropdownMenuItem<String>(
-            value: value,
-            child: Text(value),
-          );
-        }).toList(),
-      ),
-      ]
-      ),
+          DropdownButton<String>(
+            value: dropdownvalue,
+            onChanged: (String newValue) {
+              setState(() {
+                dropdownvalue = newValue;
+              });
+            },
+            items: <String>['Tower Takeover', 'Turning Point', 'In the Zone']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
+        ]),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          sku = myController.text;
+          team = myController.text;
           Navigator.push(
             context,
             MaterialPageRoute(builder: (context) => HomePage()),
@@ -102,41 +99,32 @@ class _MyCustomFormState extends State<MyCustomForm> {
     );
   }
 }
+
 class HomePage extends StatefulWidget {
   @override
   HomePageState createState() => new HomePageState();
 }
 
 class HomePageState extends State<HomePage> {
-
   List data;
 
   Future<String> getData() async {
-    if(dropdownvalue == "Turning Point") {
-      sku = "RE-VRC-18-" + sku;
-    }
-    else if(dropdownvalue == "In the Zone") {
-      sku = "RE-VRC-17-" + sku;
-    }
-    else {
-      sku = "RE-VRC-19-"+ sku;
-    }
     var response = await http.get(
-        Uri.encodeFull("http://smart-scan.mercuryrobotics.tk/tournaments/"+sku),
-        headers: {
-          "Accept": "application/json"
-        }
-    );
+        Uri.encodeFull("https://api.vexdb.io/v1/get_events?team=" +
+            team +
+            "&season=" +
+            dropdownvalue),
+        headers: {"Accept": "application/json"});
 
     this.setState(() {
-      var parsedresponse =  json.decode(response.body);
-      data = json.decode(parsedresponse['content']);
+      var parsedresponse = json.decode(response.body);
+      data = (parsedresponse['result']);
     });
     return "Success!";
   }
 
   @override
-  void initState(){
+  void initState() {
     print("Running again");
     this.getData();
   }
@@ -145,44 +133,36 @@ class HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     if (data != null) {
       return new Scaffold(
-
-        appBar: new AppBar(title: new Text("Smart Scan Rankings"),
+        appBar: new AppBar(
+            title: new Text("Smart Scan Rankings"),
             backgroundColor: Colors.blue),
-
-        body:
-        ListView.builder(
+        body: ListView.builder(
           itemCount: data == null ? 0 : data.length,
           itemBuilder: (BuildContext context, int index) {
-            return new Card(
-              child: new Text.rich(
-                TextSpan(
-                  text: (index + 1).toString() + ". " + data[index]['name'],
-                  // default text style
-                  children: <TextSpan>[
-                    TextSpan(text: '\nScore: ',
-                        style: TextStyle(fontStyle: FontStyle.italic)),
-                    TextSpan(text: (data[index]['score']).toString(),
-                        style: TextStyle(fontStyle: FontStyle.italic)),
-                  ],
-                ),
-              ),
-            );
+            return GestureDetector(
+                child: Card(
+                    child: Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(data[index]['name']))),
+                onTap: () => runSKU(data[index]['sku'], context));
           },
         ),
       );
-    }
-    else {
+    } else {
       return new Scaffold(
-
-        appBar: new AppBar(title: new Text("Smart Scan Rankings"),
+        appBar: new AppBar(
+            title: new Text("Smart Scan Rankings"),
             backgroundColor: Colors.blue),
-
-        body: Center( child: Text.rich(
+        body: Center(
+            child: Text.rich(
           TextSpan(
-            text: 'Error\n', // default text style
+            text: 'Please wait...\n\n', // default text style
             style: TextStyle(fontSize: 25),
             children: <TextSpan>[
-              TextSpan(text: 'Your request could not be processed at this time. Please try again later. ', style: TextStyle(fontStyle: FontStyle.italic, fontSize: 15)),
+              TextSpan(
+                  text:
+                      'Your request is currently being processed. If this message does not go away automatically, please try again later. ',
+                  style: TextStyle(fontStyle: FontStyle.italic, fontSize: 15)),
             ],
           ),
           textAlign: TextAlign.center,
@@ -196,25 +176,115 @@ class infoPage extends StatefulWidget {
   @override
   infoPageState createState() => new infoPageState();
 }
+
 class infoPageState extends State<infoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+          title: new Text("Smart Scan Rankings"), backgroundColor: Colors.blue),
+      body: Center(
+          child: Text.rich(
+        TextSpan(
+          text: 'Information and license\n\n', // default text style
+          style: TextStyle(fontSize: 25),
+          children: <TextSpan>[
+            TextSpan(
+                text:
+                    'Smart Scan was created by javathunderman (Arjun V.) of Vex Robotics Team 4001A. \n\nThe code for this project is available at https://github.com/javathunderman/vex-smart-scan\n\nLicensed under GNU GPL v3.0. ',
+                style: TextStyle(fontStyle: FontStyle.italic, fontSize: 15)),
+          ],
+        ),
+        textAlign: TextAlign.center,
+      )),
+    );
+  }
+}
+
+class resultsPage extends StatefulWidget {
+  @override
+  resultsPageState createState() => new resultsPageState();
+}
+
+class resultsPageState extends State<resultsPage> {
+  List data;
+
+  Future<String> getData() async {
+    var response = await http.get(
+        Uri.encodeFull(
+            "http://smart-scan.mercuryrobotics.tk/tournaments/" + sku),
+        headers: {"Accept": "application/json"});
+
+    this.setState(() {
+      var parsedresponse = json.decode(response.body);
+      data = json.decode(parsedresponse['content']);
+    });
+    return "Success!";
+  }
+
+  @override
+  void initState() {
+    print("Running again - smart scan server");
+    this.getData();
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (data != null) {
       return new Scaffold(
-
-        appBar: new AppBar(title: new Text("Smart Scan Rankings"),
+        appBar: new AppBar(
+            title: new Text("Smart Scan Rankings"),
             backgroundColor: Colors.blue),
-
-        body: Center( child: Text.rich(
+        body: ListView.builder(
+          itemCount: data == null ? 0 : data.length,
+          itemBuilder: (BuildContext context, int index) {
+            return new Card(
+              child: new Text.rich(
+                TextSpan(
+                  text: (index + 1).toString() + ". " + data[index]['name'],
+                  // default text style
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: '\nScore: ',
+                        style: TextStyle(fontStyle: FontStyle.italic)),
+                    TextSpan(
+                        text: (data[index]['score']).toString(),
+                        style: TextStyle(fontStyle: FontStyle.italic)),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      );
+    } else {
+      return new Scaffold(
+        appBar: new AppBar(
+            title: new Text("Smart Scan Rankings"),
+            backgroundColor: Colors.blue),
+        body: Center(
+            child: Text.rich(
           TextSpan(
-            text: 'Information and license\n\n', // default text style
+            text: 'Please wait...\n\n', // default text style
             style: TextStyle(fontSize: 25),
             children: <TextSpan>[
-              TextSpan(text: 'Smart Scan was created by javathunderman (Arjun V.) of Vex Robotics Team 4001A. \n\nThe code for this project is available at https://github.com/javathunderman/vex-smart-scan\n\nLicensed under GNU GPL v3.0. ', style: TextStyle(fontStyle: FontStyle.italic, fontSize: 15)),
+              TextSpan(
+                  text:
+                      'Your request is currently being processed. If this message does not go away automatically, please try again later. ',
+                  style: TextStyle(fontStyle: FontStyle.italic, fontSize: 15)),
             ],
           ),
           textAlign: TextAlign.center,
         )),
       );
     }
+  }
+}
+
+void runSKU(String skuinput, BuildContext context) {
+  sku = skuinput;
+  Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => resultsPage()),
+  );
 }
